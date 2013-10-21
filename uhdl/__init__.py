3c4e92cd10d8e7f843f4b50b5bb5841b017835ed
Keerthan Jaic <jckeerthan@gmail.com>
2013-10-21 11:03:01 -0400

    Sigals

---
+++ uhdl/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Keerthan Jaic'
+__email__ = 'jckeerthan@gmail.com'
+__version__ = '0.1.0'
+
+
+from .signals import Clock, Reset, Sig, Sigs
