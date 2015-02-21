3c93098f0b04a6af7dabec7942e6a8dcd22264b2
Keerthan Jaic <jckeerthan@gmail.com>
2015-02-21 16:20:18 -0500

    Remove author and email from __init__.py

--- uhdl/__init__.py
+++ uhdl/__init__.py
@@ -5,12 +5,13 @@ UHDL
 
 from __future__ import absolute_import
 
-__author__ = 'Keerthan Jaic'
-__email__ = 'jckeerthan@gmail.com'
 __version__ = '0.0.9'
+
+#The version udhl is currently based on.
 __myhdl__ = 'https://bitbucket.org/jck2/myhdl/get/sf-hotfixes.tar.gz#egg=myhdl'
 
+from . import math
+from . import models
 from .constructors import bits, randbits, create, Sig, Sigs
 from .sim import Clock, Reset, run
 from .hw import HW
-from . import math
