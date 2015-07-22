d257858a4906ae6b3f03b75140611fece97fbae2
Keerthan Jaic <jckeerthan@gmail.com>
2015-07-22 02:49:07 -0400

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import ast
-import re
-
 from setuptools import setup
 
 
-# _version_re = re.compile(r'__version__\s+=\s+(.*)')
+reqs = [
+    'myhdl>=0.9.0',
+    'click',
+    'wrapt'
+]
+test_reqs = ['pytest', 'hypothesis']
 
-# with open('uhdl/__init__.py', 'rb') as f:
-#     version = str(ast.literal_eval(_version_re.search(
-#         f.read().decode('utf-8')).group(1)))
-
-
-readme = open('README.md').read()
-
-tests_require = ['pytest', 'hypothesis']
+requires = {
+    'setup_requires': ['setuptools_scm'],
+    'install_requires': reqs,
+    'tests_require': test_reqs,
+    'extras_require': {
+        'testing': test_reqs,
+    }
+}
 
 setup(
     name='uhdl',
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
     description='Python Hardware Description for Humans.',
-    long_description=readme,
+    long_description=open('README.md').read(),
+    url='https://github.com/jck/uhdl',
     author='Keerthan Jaic',
     author_email='jckeerthan@gmail.com',
-    url='https://github.com/jck/uhdl',
-    packages=[
-        'uhdl',
-        'uhdl.backends'
-    ],
-    package_dir={'uhdl': 'uhdl'},
+    license="BSD",
+    packages=['uhdl'],
     entry_points={
         'console_scripts': [
             'uhdl = uhdl.cli:cli'
         ]
     },
-    include_package_data=True,
-    install_requires=[
-        'myhdl>=0.9.0',
-        'click',
-        'wrapt',
-    ],
-    tests_require=tests_require,
-    extras_require = {'test': tests_require},
-    license="BSD",
     zip_safe=False,
-    keywords='uhdl',
     classifiers=[
         'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)'
         'Development Status :: 1 - Planning',
@@ -58,4 +43,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
+    keywords='myhdl uhdl',
+    **requires
 )
